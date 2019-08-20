42eac1dd968cafb5a89b99b89057efd250121743
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-20 11:18:28 +0800

    fix

--- contrib/tvmop/basic/ufunc.py
+++ contrib/tvmop/basic/ufunc.py
@@ -31,7 +31,8 @@ def compute_add(dtype, ndim):
        dtype=AllTypes, ndim=list(range(1, 6)))
 def vadd(dtype, ndim):
     s, A, B, C = compute_add(dtype, ndim)
-    s[C].parallel(C.op.axis[0], nparts=16)
+    bx, tx = s[C].split(C.op.axis[0], nparts=16)
+    s[C].parallel(bx)
     # axes = [axis for axis in C.op.axis]
     # fused = s[C].fuse(*axes)
     # bx, tx = s[C].split(fused, nparts=16)
