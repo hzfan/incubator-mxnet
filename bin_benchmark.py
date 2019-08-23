f817cc6273abafb2cebfca6c1fd73755b2a20c82
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-23 17:34:17 +0800

    fix

--- bin_benchmark.py
+++ bin_benchmark.py
@@ -34,8 +34,8 @@ def measure_cost(repeat, func_name, *args, **kwargs):
 
 def test_add():
     # tvm add
-    n = 1024
-    m = 1024
+    n = 1023
+    m = 1023
     print("tvm add:")
     a = mx.nd.random.uniform(shape=(n, m), dtype='float32')
     b = mx.nd.random.uniform(shape=(n, m), dtype='float32')
