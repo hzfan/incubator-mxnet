3aecd694d4d23d3796ac7db1d51c8859e64c1b87
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-23 17:25:13 +0800

    fix

--- bin_benchmark.py
+++ bin_benchmark.py
@@ -34,8 +34,8 @@ def measure_cost(repeat, func_name, *args, **kwargs):
 
 def test_add():
     # tvm add
-    n = 1023
-    m = 1023
+    n = 1025
+    m = 1025
     print("tvm add:")
     a = mx.nd.ones(n * m).reshape(n, m)
     b = mx.nd.ones(m).reshape(1, m)
