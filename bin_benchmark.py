cf2009be5fab37ced6e7bd02da50ab906560ec98
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-23 17:34:57 +0800

    fix

--- bin_benchmark.py
+++ bin_benchmark.py
@@ -34,18 +34,18 @@ def measure_cost(repeat, func_name, *args, **kwargs):
 
 def test_add():
     # tvm add
-    n = 1023
-    m = 1023
+    n = 1024
+    m = 1024
     print("tvm add:")
     a = mx.nd.random.uniform(shape=(n, m), dtype='float32')
     b = mx.nd.random.uniform(shape=(n, m), dtype='float32')
-    cost = measure_cost(50000, mx.nd.contrib.tvm_vadd, a, b)
+    cost = measure_cost(5000, mx.nd.contrib.tvm_vadd, a, b)
     print("cost: {} ms".format(cost * 1000))
     # np add
     print("np add:")
     a = np.array(mx.nd.random.uniform(shape=(n, m)), dtype='float32')
     b = np.array(mx.nd.random.uniform(shape=(n, m)), dtype='float32')
-    cost = measure_cost(50000, np.add, a, b)
+    cost = measure_cost(5000, np.add, a, b)
     print("cost: {} ms".format(cost * 1000))
 
 
