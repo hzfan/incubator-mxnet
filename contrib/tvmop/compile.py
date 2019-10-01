16c9742bd668791b9697b365398d2b80ec87bfab
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-10-02 02:56:37 +0800

    fix

--- contrib/tvmop/compile.py
+++ contrib/tvmop/compile.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
                         help="Target path which stores compiled library")
     arguments = parser.parse_args()
 
-    autotvm.measure.measure_methods.set_cuda_target_arch("-gencode=arch=sm_35,code=sm_35")
+    autotvm.measure.measure_methods.set_cuda_target_arch("-gencode=arch=compute_37,code=sm_37")
 
     func_list_llvm = []
     func_list_cuda = []
